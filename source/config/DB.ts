aedabd88679d04baa15076a876d527bcc26050a6
Ankan Saha <ankansahadaluabari@gmail.com>
2024-10-20 03:24:10 +0530

    Refactor DB.ts to export FileManager and FolderManager

--- source/config/DB.ts
+++ source/config/DB.ts
@@ -1,10 +1,14 @@
 // Import All Required Sub Modules
 import { SchemaTypes } from "../Models/DataTypes.models";
+import FileManager from "../Storage/FileManager";
+import FolderManager from "../Storage/FolderManager";
 
 // Export Specific Modules
-export { SchemaTypes };
+export { SchemaTypes, FileManager, FolderManager };
 
 // Export With All Sub Modules
 export default {
   SchemaTypes,
+  FileManager,
+  FolderManager,
 };
