8c446ed33affb2009d46663049177161667dc187
Ankan Saha <ankansahadaluabari@gmail.com>
2024-10-20 12:46:26 +0530

    Refactor Configuration class and DB.ts
    - Update Configuration class to include encryption functionality
    - Add default keys to DB.ts for DBMS name and file extension

--- source/Operation/Indexation.operation.ts
+++ source/Operation/Indexation.operation.ts
@@ -1,1 +1,49 @@
-export default class Configuration {}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+// Import Libraries
+import { DBMS_Name } from "../config/DB";
+export default class Configure {
+    // Properties
+    private readonly Sheema: object | any;
+    private isEncrypted: boolean;
+    private encryptionKey: string;
+
+    constructor(Sheema: object|any, isEncrypted: boolean = false, ) {
+        this.Sheema = Sheema;
+        this.isEncrypted = isEncrypted;
+        this.encryptionKey = DBMS_Name;
+
+    }
+
+    // Configure Methos
+    public getSchema(): object | any {
+        return this.Sheema;
+    }
+
+    public EncryptionStatus(): boolean {
+        return this.isEncrypted;
+    }
+
+    public getEncryptionKey(): string {
+        return this.encryptionKey;
+    }
+
+    public setEncryptionKey(key: string): void {
+        this.encryptionKey = key;
+    }
+
+    public setEncryptionStatus(status: boolean): void {
+        this.isEncrypted = status;
+    }
+
+    public getConfiguration(): object | any {
+        return {
+            Schema: this.getSchema(),
+            Encryption: {
+                Status: this.EncryptionStatus(),
+                Key: this.getEncryptionKey(),
+            }
+        }
+    }
+
+}
--- source/config/DB.ts
+++ source/config/DB.ts
@@ -3,6 +3,10 @@ import { SchemaTypes } from "../Models/DataTypes.models";
 import FileManager from "../Storage/FileManager";
 import FolderManager from "../Storage/FolderManager";
 
+// Default Keys
+export const DBMS_Name: string = "AxioDB";
+export const DBMS_File_EXT: string = ".axiodb";
+
 // Export Specific Modules
 export { SchemaTypes, FileManager, FolderManager };
 
