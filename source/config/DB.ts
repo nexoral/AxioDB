32a8506237614da0bacf2a6437714700679e2d62
Ankan Saha <ankansahadaluabari@gmail.com>
2024-12-18 01:15:39 +0530

    Added Premature Insert Feature

--- source/config/DB.ts
+++ source/config/DB.ts
@@ -1,13 +1,15 @@
 // Import All Required Sub Modules
+import {General} from "./Keys/Keys";
 import { SchemaTypes } from "../Models/DataTypes.models";
 import schemaValidate from "../Models/validator.models";
 import Configure from "../Operation/Indexation.operation";
 // Web Server Configuration
 import WebServer from "../Web/Fastify";
 
+
 // Initialize Configuration Cluster in start
 WebServer().then((r) => console.log(r));
-new Configure().CreateTreeRoot().then((r) => console.log(r));
+new Configure(General.DBMS_Name).CreateTreeRoot().then((r) => console.log(r));
 
 // Export Specific Modules
 export { SchemaTypes, schemaValidate, Configure };
