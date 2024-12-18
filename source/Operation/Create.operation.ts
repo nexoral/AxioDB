43eb982e9835b8b1547ccf1a31093f76c26b62a1
Ankan Saha <ankansahadaluabari@gmail.com>
2024-12-18 23:09:23 +0530

    Fixed The Documentation Issue

--- source/Operation/Create.operation.ts
+++ source/Operation/Create.operation.ts
@@ -1,44 +1,76 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import FileManager from "../Storage/FileManager";
-import { ClassBased } from "outers";
+import {ClassBased} from "outers";
 import responseHelper from "../Helper/response.helper";
 
+/**
+ * Class representing an insertion operation.
+ */
 export default class Insertion {
-  private readonly Operator: FileManager;
-  private readonly collectionName: string;
-  private readonly data: object | any;
+    private readonly Operator: FileManager;
+    private readonly collectionName: string;
+    private readonly data: object | any;
+    private documentId: string | null;
 
-  constructor(collectionName: string, data: object | any) {
-    this.Operator = new FileManager();
-    this.collectionName = collectionName;
-    this.data = data;
-  }
-
-  public async Save(): Promise<any> {
-    let isExist;
-    let ID;
-    do {
-      ID = new ClassBased.UniqueGenerator(15).RandomWord(true);
-      const response = await this.Operator.FileExists(
-        `${this.collectionName}/${ID}.json`,
-      );
-      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-      response.status ? (isExist = true) : (isExist = false);
-    } while (isExist == false);
-
-    const response = await this.Operator.WriteFile(
-      `${this.collectionName}/${ID}.json`,
-      JSON.stringify(this.data),
-    );
-
-    if (response.status) {
-      return await new responseHelper().Success({
-        Message: "Data Saved Successfully",
-        Data: {
-          ID,
-          ...this.data,
-        },
-      });
+    /**
+     * Creates an instance of Insertion.
+     * @param {string} collectionName - The name of the collection.
+     * @param {object | any} data - The data to be inserted.
+     */
+    constructor(collectionName: string, data: object | any) {
+        this.Operator = new FileManager();
+        this.collectionName = collectionName;
+        this.data = data;
+        this.documentId = null;
+        this.setDocumentID();
     }
-  }
+
+    /**
+     * Saves the data to a file.
+     * @returns {Promise<any>} A promise that resolves with the response of the save operation.
+     */
+    public async Save(): Promise<any> {
+        const response = await this.Operator.WriteFile(
+            `${this.collectionName}/${this.documentId}.json`,
+            JSON.stringify(this.data),
+        );
+
+        if (response.status) {
+            return await new responseHelper().Success({
+                Message: "Data Inserted Successfully",
+                DocumentID: this.documentId,
+                Data: {
+                    ...this.data
+                }
+            });
+        }
+    }
+
+    /**
+     * Generates a unique document ID.
+     * @returns {Promise<string>} A promise that resolves with a unique document ID.
+     */
+    private async generateUniqueDocumentId(): Promise<string> {
+        let isExist;
+        let ID;
+        do {
+            ID = new ClassBased.UniqueGenerator(15).RandomWord(true);
+            const response = await this.Operator.FileExists(
+                `${this.collectionName}/${ID}.json`,
+            );
+          // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+            response.status ? (isExist = true) : (isExist = false);
+        } while (isExist == false);
+
+        return ID;
+    }
+
+    /**
+     * Sets the document ID by generating a unique ID.
+     */
+    private setDocumentID() {
+        this.generateUniqueDocumentId().then((ID) => {
+            this.documentId = ID;
+        });
+    }
 }
--- source/Operation/Indexation.operation.ts
+++ source/Operation/Indexation.operation.ts
@@ -6,6 +6,9 @@ import { General } from "../config/Keys/Keys";
 import path from "path";
 import Insertion from "./Create.operation";
 
+/**
+ * Class representing the AxioDB database.
+ */
 export default class AxioDB {
   // Properties
   private readonly collectionName: string; // Private Property
@@ -16,6 +19,13 @@ export default class AxioDB {
   private readonly currentPATH: string; // Private Property
   private collectionPath: string; // Private Property
 
+  /**
+   * Creates an instance of AxioDB.
+   * @param {string} collectionName - The name of the collection.
+   * @param {object | any} [Schema] - The schema object defining the structure of the collection.
+   * @param {boolean} [isEncrypted=false] - Indicates if the collection is encrypted.
+   * @param {string} [ClusterName=General.DBMS_Name] - The name of the cluster.
+   */
   constructor(
     collectionName: string,
     Schema?: object | any,
@@ -32,26 +42,51 @@ export default class AxioDB {
   }
 
   // Configure Method
+
+  /**
+   * Gets the schema of the collection.
+   * @returns {object | any} The schema of the collection.
+   */
   public getSchema(): object | any {
     return this.Schema;
   }
 
+  /**
+   * Gets the encryption status of the collection.
+   * @returns {boolean} The encryption status of the collection.
+   */
   public EncryptionStatus(): boolean {
     return this.isEncrypted;
   }
 
+  /**
+   * Gets the encryption key of the collection.
+   * @returns {string} The encryption key of the collection.
+   */
   public getEncryptionKey(): string {
     return this.#encryptionKey;
   }
 
+  /**
+   * Sets the encryption key of the collection.
+   * @param {string} key - The new encryption key.
+   */
   public setEncryptionKey(key: string): void {
     this.#encryptionKey = key;
   }
 
+  /**
+   * Sets the encryption status of the collection.
+   * @param {boolean} status - The new encryption status.
+   */
   public setEncryptionStatus(status: boolean): void {
     this.isEncrypted = status;
   }
 
+  /**
+   * Gets the configuration of the collection.
+   * @returns {object | any} The configuration of the collection.
+   */
   public getConfiguration(): object | any {
     return {
       Schema: this.getSchema(),
@@ -63,11 +98,18 @@ export default class AxioDB {
   }
 
   // Public Functions
+
+  /**
+   * Inserts data into the collection.
+   * @param {object | any} data - The data to be inserted.
+   * @returns {Promise<void>} A promise that resolves when the data is inserted.
+   */
   public async Insert(data: object | any): Promise<void> {
     await new Insertion(this.collectionName, data).Save();
   }
 
   // Internal Functions
+
   /**
    * Creates the root directory for the tree structure.
    *
@@ -82,6 +124,10 @@ export default class AxioDB {
     );
   }
 
+  /**
+   * Creates the collection directory.
+   * @returns {Promise<void>} A promise that resolves when the collection directory is created.
+   */
   private async createCollection(): Promise<void> {
     if (this.isEncrypted) {
       await new FolderManager().CreateDirectory(
