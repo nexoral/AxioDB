4b1ccc874a5492904e391e69b78b742394bcba67
Ankan Saha <ankansahadaluabari@gmail.com>
2024-12-31 00:21:23 +0530

    feat: add web server configuration and update server port usage

--- source/config/Keys/Keys.ts
+++ source/config/Keys/Keys.ts
@@ -2,3 +2,9 @@ export enum General {
   DBMS_Name = "AxioDB",
   DBMS_File_EXT = ".axiodb",
 }
+
+
+// Web Server Configuration
+export enum WebServer {
+  ServerPORT = 2025,
+}
--- source/server/Keys/Keys.ts
+++
@@ -1,3 +0,0 @@
-export enum General {
-  PORT = 27018,
-}
